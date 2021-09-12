aef83ddc094fbc80425a5b2f38c86bcdae7f8c38
AqilahMalik <aqilahsimaa98@gmail.com>
2021-09-12 22:30:04 +0800

    update styl3s_

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -28,54 +28,56 @@ function Forecast(props) {
             {f_city}, {f_country} <br /> {f_date}
           </span>
         </h2>
-        <div className="f_forecast">
-          <div className="card_forecast">
-            {" "}
-            10:00 am <br /> <hr />
-            <div className="currentDetail_value">
-              {f_maxtemp1}&deg;C <br />
-              {f_mintemp1}&deg;F <br />
-              {f_cond1}
+        <div className="entryPoint1">
+          <div className="f_forecast">
+            <div className="card_forecast">
+              {" "}
+              10:00 am <br /> <hr />
+              <div className="currentDetail_value">
+                {f_maxtemp1}&deg;C <br />
+                {f_mintemp1}&deg;F <br />
+                {f_cond1}
+              </div>
             </div>
-          </div>
 
-          <div className="card_forecast">
-            {" "}
-            04:00 pm <br /> <hr />
-            <div className="currentDetail_value">
-              {f_maxtemp2}&deg;C <br />
-              {f_mintemp2}&deg;F <br />
-              {f_cond2}
+            <div className="card_forecast">
+              {" "}
+              04:00 pm <br /> <hr />
+              <div className="currentDetail_value">
+                {f_maxtemp2}&deg;C <br />
+                {f_mintemp2}&deg;F <br />
+                {f_cond2}
+              </div>
             </div>
-          </div>
 
-          <div className="card_forecast">
-            {" "}
-            10:00 pm <br /> <hr />
-            <div className="currentDetail_value">
-              {f_maxtemp3}&deg;C <br />
-              {f_mintemp3}&deg;F <br />
-              {f_cond3}
+            <div className="card_forecast">
+              {" "}
+              10:00 pm <br /> <hr />
+              <div className="currentDetail_value">
+                {f_maxtemp3}&deg;C <br />
+                {f_mintemp3}&deg;F <br />
+                {f_cond3}
+              </div>
             </div>
-          </div>
 
-          <div className="card_forecast">
-            {" "}
-            04:00 am <br /> <hr />
-            <div className="currentDetail_value">
-              {f_maxtemp4}&deg;C <br />
-              {f_mintemp4}&deg;F <br />
-              {f_cond4}
+            <div className="card_forecast">
+              {" "}
+              04:00 am <br /> <hr />
+              <div className="currentDetail_value">
+                {f_maxtemp4}&deg;C <br />
+                {f_mintemp4}&deg;F <br />
+                {f_cond4}
+              </div>
             </div>
-          </div>
 
-          <div className="card_forecast">
-            {" "}
-            07:00 am <br /> <hr />
-            <div className="currentDetail_value">
-              {f_maxtemp5}&deg;C <br />
-              {f_mintemp5}&deg;F <br />
-              {f_cond5}
+            <div className="card_forecast">
+              {" "}
+              07:00 am <br /> <hr />
+              <div className="currentDetail_value">
+                {f_maxtemp5}&deg;C <br />
+                {f_mintemp5}&deg;F <br />
+                {f_cond5}
+              </div>
             </div>
           </div>
         </div>
