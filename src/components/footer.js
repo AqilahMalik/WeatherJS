745a68a21100b3730517f75acba20f361cb67882
AqilahMalik <aqilahsimaa98@gmail.com>
2021-09-13 13:27:19 +0800

    more icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,71 +7,111 @@ function Footer() {
 
         <h3>
           <br />
-          <i className={`wi wi-snowflake-cold display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-snowflake-cold display-5`}></i>
+          </div>
+          <div className="hide">Cool!</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-humidity display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-lightning display-5`}></i>
+          </div>
+          <div className="hide">Lightning!</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-fog display-5 `}></i>
+          <div className="myDIV">
+            <i className={`wi wi-flood display-5 `}></i>
+          </div>
+          <div className="hide">Flood!</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-stars display-5 `}></i>
+          <div className="myDIV">
+            <i className={`wi wi-stars display-5 `}></i>
+          </div>
+          <div className="hide">Stars!</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-windy display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-time-11 display-5`}></i>
+          </div>
+          <div className="hide">Clock</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-humidity display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-humidity display-5`}></i>
+          </div>
+          <div className="hide">Humidity</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-fog display-5 `}></i>
+          <div className="myDIV">
+            <i className={`wi wi-fog display-5 `}></i>
+          </div>
+          <div className="hide">Fog clouds</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-night-alt-rain-wind display-5 `}></i>
+          <div className="myDIV">
+            <i className={`wi wi-night-alt-rain-wind display-5 `}></i>
+          </div>
+          <div className="hide">Raining!</div>
         </h3>
       </div>
+
       <div className="wrapper_footer">
         <br />
         <br />
 
         <h3>
           <br />
-          <i className={`wi wi-windy display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-windy display-5`}></i>
+          </div>
+          <div className="hide">Windy!</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-celsius display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-celsius display-5`}></i>
+          </div>
+          <div className="hide">Temperatue</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-earthquake display-5 `}></i>
+          <div className="myDIV">
+            <i className={`wi wi-earthquake display-5 `}></i>
+          </div>
+          <div className="hide">Eartquake!</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-thermometer display-5 `}></i>
+          <div className="myDIV">
+            <i className={`wi wi-thermometer display-5 `}></i>
+          </div>
+          <div className="hide">Thermometer</div>
         </h3>
         <br />
         <h3>
           <br />
-          <i className={`wi wi-smoke display-5`}></i>
+          <div className="myDIV">
+            <i className={`wi wi-smoke display-5`}></i>
+          </div>
+          <div className="hide">Smoke!</div>
         </h3>
         <br />
       </div>
